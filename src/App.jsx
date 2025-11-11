542a84bab953f5c129fe3a1eec2153134367deb9
NataniMoraes <natanigabriele@gmail.com>
2025-11-10 21:32:30 -0300

    Inicialização do projeto Web
    Inicialização do projeto Web

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, Heading } from '@chakra-ui/react' // 1. Importar componentes
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    // Box é como uma <div>, mas com superpoderes
+    <Box bg='pink.100' w='100%' h='100vh' p={4}> 
+      {/* Heading é um <h1>, <h2>, etc. */}
+      <Heading color='purple.700'>
+        Bem-vindo(a) à Papelaria Candy!
+      </Heading>
+    </Box>
   )
 }
 
