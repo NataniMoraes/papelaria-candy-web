c5ee1e8bced4b6e9ec00dabe86bc816283b3047e
NataniMoraes <natanigabriele@gmail.com>
2025-11-10 21:44:17 -0300

    Continuação

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
-import { Box, Heading } from '@chakra-ui/react' // 1. Importar componentes
+import { Box, Flex, Heading, Text } from '@chakra-ui/react'
 
 function App() {
   return (
-    // Box é como uma <div>, mas com superpoderes
-    <Box bg='pink.100' w='100%' h='100vh' p={4}> 
-      {/* Heading é um <h1>, <h2>, etc. */}
-      <Heading color='purple.700'>
-        Bem-vindo(a) à Papelaria Candy!
-      </Heading>
-    </Box>
+    <Flex w='100%'> {/* Container principal em Flexbox */}
+      
+      {/* 1. SIDEBAR (Navegação) */}
+      <Box 
+        w='300px' // Largura fixa da sidebar
+        h='100vh' // Altura total da tela
+        bg='gray.100' // Cor de fundo pastel
+        p={6} // Padding (espaçamento interno)
+      >
+        <Heading as='h1' size='lg' color='purple.700' mb={8}> {/* Título */}
+          Papelaria Candy 🍬
+        </Heading>
+
+        {/* Links de Navegação (futuros) */}
+        <Box mb={4}>
+          <Text fontSize='xl' fontWeight='bold' color='gray.600'>Home</Text>
+        </Box>
+        <Box mb={4}>
+          <Text fontSize='xl' fontWeight='bold' color='gray.600'>Produtos</Text>
+        </Box>
+        <Box mb={4}>
+          <Text fontSize='xl' fontWeight='bold' color='gray.600'>Categorias</Text>
+        </Box>
+        <Box mb={4}>
+          <Text fontSize='xl' fontWeight='bold' color='gray.600'>Vendas</Text>
+        </Box>
+      </Box>
+
+      {/* 2. ÁREA DE CONTEÚDO PRINCIPAL */}
+      <Box 
+        flex='1' // Faz esta Box ocupar todo o espaço restante
+        h='100vh'
+        p={10} // Padding maior para o conteúdo
+        bg='pink.50' // Fundo rosa bem clarinho
+      >
+        <Heading>Página Atual</Heading>
+        <Text>O conteúdo da página será carregado aqui.</Text>
+      </Box>
+
+    </Flex>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { ChakraProvider } from '@chakra-ui/react' // Importante!
+import { ChakraProvider } from '@chakra-ui/react' // 1. Importar
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* O <App /> DEVE estar dentro do <ChakraProvider> */}
+    {/* 2. Envolver o <App /> com o Provider */}
     <ChakraProvider>
       <App />
     </ChakraProvider>
